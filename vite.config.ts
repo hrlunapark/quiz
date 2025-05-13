954ddd229c761e0d9771627ce8359284e7ec23fd
erutuche <ya.petrova.nadya@gmail.com>
2025-05-13 17:28:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lunaparkmlquiz/',
+  base: '/quiz/',
   assetsInclude: ['**/*.jpg', '**/*.png', '**/*.webp'],
 })
